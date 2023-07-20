cf345076211f3d69ca6c701483b2f3d83ec43e43
Abiel Zulio M <abielzm@gmail.com>
2023-07-20 19:30:04 +0700

    feat: `song.entity`

---
+++ domain/entities/song.entity.ts
@@ -0,0 +1,26 @@
+interface ISong {
+  id: string
+  title: string
+  artist: string
+  url: string
+  is_playing: boolean
+  playing_count: number
+}
+
+class Song implements ISong {
+  public id: ISong["id"]
+  public title: ISong["title"]
+  public artist: ISong["artist"]
+  public url: ISong["url"]
+  public is_playing: ISong["is_playing"]
+  public playing_count: ISong["playing_count"]
+
+  constructor(song: ISong) {
+    this.id = song.id
+    this.title = song.title
+    this.artist = song.artist
+    this.url = song.url
+    this.is_playing = song.is_playing
+    this.playing_count = song.playing_count
+  }
+}
